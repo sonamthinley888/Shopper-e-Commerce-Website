ffe375a2bfd913196ee6dabe17f98a81bed70df2
Sonam Thinley <sonamthinley888@gmail.com>
2024-08-04 23:24:19 +1000

    Deleted Duplicate Assets and initialized backend works

---
+++ backend/index.js
@@ -0,0 +1,53 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+const { log } = require("console");
+
+app.use(express.json());
+app.use(cors());
+
+// Database Connection with MongoDB
+mongoose.connect("mongodb+srv://coder_91:Stj_mongo24@cluster0.ln6blra.mongodb.net/e-commerce")
+
+// API Creation
+app.get("/", (req,res)=>{
+    res.send("Express App is running")
+})
+
+// Image Storage Engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+
+    }
+})
+
+const upload = multer({storage:storage})
+
+//Creating Upload Endpoit for Images
+
+app.use('/images',express.static('upload/images'))
+
+app.post("/upload", upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+
+
+app.listen(port,(error)=>{
+    if (!error){
+        console.log("Server Running on Port "+port)
+    }
+    else{
+        console.log("Error : "+error)
+    }
+})
