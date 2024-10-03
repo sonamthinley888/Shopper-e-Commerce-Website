dd795a25b96782092ec7d9acab31d2c9f8d4816e
Sonam Thinley <sonamthinley888@gmail.com>
2024-10-03 23:21:13 +1000

    CSS for Product details

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -15,6 +15,18 @@ const ListProduct = () => {
       fetchInfo();
     },[])
 
+    const remove_product = async (id) => {
+      await fetch('http://localhost:4000/removeproduct',{
+        method:'POST',
+        headers:{
+          Accept:'application/json',
+          'Content-Type':'application/json',
+        },
+        body:JSON.stringify({id:id})
+      })
+      await fetchInfo();
+      
+    }
 
 
   return (
@@ -31,14 +43,16 @@ const ListProduct = () => {
       <div className="listproduct-allproducts">
         <hr />
         {allproducts.map((product,index)=>{
-          return <div key={index} className="listproduct-format-main listproduct-format">
+          return <><div key={index} className="listproduct-format-main listproduct-format">
             <img src={product.image} alt="" className="listproduct-product-icon" />
             <p>{product.name}</p>
             <p>${product.old_price}</p>
             <p>${product.new_price}</p>
             <p>{product.category}</p>
-            <img src={cross_icon} alt="" className="listproduct-remove-icon" />
+            <img onClick={()=>{remove_product(product.id)}} src={cross_icon} alt="" className="listproduct-remove-icon" />
           </div>
+          <hr />
+          </>
         })}
         
       </div>
