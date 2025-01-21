44d6e6ea35980092b70e9c1368a73c71153511d7
Sonam Thinley <sonamthinley888@gmail.com>
2025-01-21 21:55:47 +1100

    updated front end new collection to work with render

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,29 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import './NewCollections.css'
+import React, { useEffect, useState } from 'react';
+import './NewCollections.css';
 
-import Item from '../Item/Item'
+import Item from '../Item/Item';
 
 const NewCollections = () => {
-
   const [new_collection, setNew_Collection] = useState([]);
-  
-  useEffect(()=>{
-    fetch('http://localhost:4000/newcollections')
-    .then((response)=>response.json())
-    .then((data)=>setNew_Collection(data));
-  },[])
+
+  useEffect(() => {
+    fetch('https://shopper-e-commerce-website-jheh.onrender.com')  // Update this to your Render backend URL
+      .then((response) => response.json())
+      .then((data) => setNew_Collection(data));
+  }, []);
 
   return (
     <div className='new-collections'>
-        <h1>NEW COLLECTIONS</h1>
-        <hr />
-        <div className="collections">
-            {new_collection.map((item,i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-            })}
-        </div>
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className="collections">
+        {new_collection.map((item, i) => (
+          <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewCollections
+export default NewCollections;
